20df200fc8d8981d0a105e1644fdea0f70aa155c
cj2hi <cj2hi@qq.com>
2016-08-03 16:25:45 +0800

    add webView ...

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_linerar_layout);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://www.baidu.com");
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+                //返回值是true时为webview打开
+                return true;
+            }
+        });
+
     }
 }
