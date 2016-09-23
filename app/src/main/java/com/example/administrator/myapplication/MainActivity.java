3bd42947b690db25ee23634c3c029ade894f091a
cj2hi <cj2hi@qq.com>
2016-09-23 18:09:34 +0800

    加入弹出窗口

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,14 +5,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -21,16 +26,20 @@ import java.io.File;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button; // 点击开始下载文件
     private DownloadManager dm;
-    private long mTaskId;
+    private long mTaskId; // 下载任务ID号
+    private TextView mTextViewShowPopWindow; // 点击显示PopMenu
+    private PopupWindow mPopupWindow;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_linerar_layout);
+
+        mTextViewShowPopWindow = (TextView) findViewById(R.id.tv_show_popmenu);
+        mTextViewShowPopWindow.setOnClickListener(new textClick());
 
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.loadUrl("http://www.baidu.com");
@@ -47,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //下载文件
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,14 +65,15 @@ public class MainActivity extends AppCompatActivity {
                 String url = "http://sw.bos.baidu.com/sw-search-sp/software/6c7303b41fffa/BaiduPlayer5_5.3.1.44_Setup_2.exe";
                 DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
                 request.setAllowedOverRoaming(false);// 设置漫游网络是否下载
-
+//                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);// 设置只在WIFI下才下载
                 String filename = url.substring(url.lastIndexOf("/") + 1);
+                // 设置保存在当前程序下的DOWNLOAD目录下
                 request.setDestinationInExternalFilesDir(getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);
 
                 // 得到下载管理器
                 dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 
-                // 将下载任务加入队列,得到ID,可能通过ID控制下载任务
+                // 将下载任务加入队列,得到ID,可通过ID控制下载任务
                 mTaskId = dm.enqueue(request);
 
                 //注册广播接收者，监听下载状态
@@ -71,6 +82,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        // 设置弹出窗口
+        mPopupWindow = new PopupWindow(ViewGroup.LayoutParams.MATCH_PARENT,
+                 ViewGroup.LayoutParams.WRAP_CONTENT);
+        mPopupWindow.setContentView(getLayoutInflater().inflate(R.layout.activity_pop, null));
+        mPopupWindow.setFocusable(true);//取得焦点
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        mPopupWindow.setAnimationStyle(R.style.pop_menu_anim); // 设置动画
+
+
+
 
     }
 
@@ -118,5 +141,16 @@ public class MainActivity extends AppCompatActivity {
         //在服务中开启activity必须设置flag
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    // TextView点击事件
+    private class textClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            //显示PopupWindow
+            mPopupWindow.showAtLocation(v, Gravity.BOTTOM,0,0);
+
+        }
     }
 }
